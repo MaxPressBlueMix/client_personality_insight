0270c0a3958062b386781521cc2a26783ee2f27f
David Powell <david@depowell.com>
2015-08-07 22:06:51 -0500

    beginnings of personality graph

--- Client_Personality_Insight/cpi.js
+++ Client_Personality_Insight/cpi.js
@@ -1,6 +1,48 @@
 if (Meteor.isClient) {
 	Session.set('pageNumber',1);
 	Session.set("userprofile",null);
+
+	function buildGraph()
+		{
+		
+		}
+	
+	function bumpPage(event, template) 
+		{
+		event.preventDefault(); // We'll handle it
+		event.stopPropagation();
+		if (event.target.name=="prev")
+			decreasePageNumber();//slide in previous page
+		else if (event.target.name=="next")
+			increasePageNumber();//slide in next page
+		else if (event.target.name=="fetch")
+			increasePageNumber();//slide in next page
+		return false;
+		}
+	
+	function openLookup(event) 
+		{
+		console.log(event);
+		event.preventDefault(); // We'll handle it
+		event.stopPropagation();
+		// Look up the client's twitter handle
+		document.getElementById("overlay").style.visibility ="visible";
+		document.getElementById("twitter-handle-lookup").className += "slideExpandUp";
+		return false;
+		}
+	
+	function closeLookup()
+		{
+		document.getElementById("overlay").style.visibility ="hidden";
+		document.getElementById("twitter-handle-lookup").className =
+			   document.getElementById("twitter-handle-lookup").className.replace
+			      ( /(?:^|\s)slideExpandUp(?!\S)/g , '' )
+		}
+	
+	function comparePageNums(pageNum) 
+		{
+		return Session.get('pageNumber')==pageNum;
+		}
 	
 	function getTwitterUser()
 		{
@@ -47,30 +89,41 @@ if (Meteor.isClient) {
 	function increasePageNumber()
 		{
 		var page=Session.get('pageNumber');
-    	document.getElementById("page"+page).style.zIndex ="1"; //old page
-    	document.getElementById("page"+page).style.visibility ="hidden"; //hide it
-    	document.getElementById("page"+page).className =	//remove old slider
- 		   document.getElementById("page"+page).className.replace
+		var whichPage=document.getElementById("page"+page);
+    	whichPage.style.zIndex ="1"; //old page
+    	whichPage.style.visibility ="hidden"; //hide it
+    	whichPage.className =	//remove old slider
+    		whichPage.className.replace
  		      ( /(?:^|\s)slideRight(?!\S)/g , '' )
 		Session.set('pageNumber',++page);
-    	document.getElementById("page"+page).style.visibility ="visible"; //show it
-    	document.getElementById("page"+page).style.zIndex ="10"; //new page
-    	document.getElementById("page"+page).className += " slideLeft"; //slide it
+    	whichPage=document.getElementById("page"+page);
+    	if (whichPage==null)
+    		{
+    		Session.set('pageNumber',--page);
+    		}
+    	else
+    		{
+	    	whichPage.style.visibility ="visible"; //show it
+	    	whichPage.style.zIndex ="10"; //new page
+	    	whichPage.className += " slideLeft"; //slide it
+    		}
 		}
 	
 	function decreasePageNumber()
 		{
 		var page=Session.get('pageNumber');
 		if (page==1) return;
-    	document.getElementById("page"+page).style.zIndex ="1"; //old page
-    	document.getElementById("page"+page).style.visibility ="hidden"; //hide it
-    	document.getElementById("page"+page).className =	//remove old slider
-  		document.getElementById("page"+page).className.replace
+		var whichPage=document.getElementById("page"+page);
+		whichPage.style.zIndex ="1"; //old page
+		whichPage.style.visibility ="hidden"; //hide it
+		whichPage.className =	//remove old slider
+			whichPage.className.replace
   		      ( /(?:^|\s)slideLeft(?!\S)/g , '' )
 		Session.set('pageNumber',--page);
-    	document.getElementById("page"+page).style.visibility ="visible"; //show it
-    	document.getElementById("page"+page).style.zIndex ="10"; //new page
-    	document.getElementById("page"+page).className += " slideRight"; //slide it
+		whichPage=document.getElementById("page"+page);
+		whichPage.style.visibility ="visible"; //show it
+		whichPage.style.zIndex ="10"; //new page
+		whichPage.className += " slideRight"; //slide it
 		}
 
 	Template.page.helpers({
@@ -86,12 +139,9 @@ if (Meteor.isClient) {
 		    }
 		  },
 		
-	pageIs: function(pageNum) 
-		{
-		return Session.get('pageNumber')==pageNum;
-		},
-		
-	userProfile: getTwitterUser
+		pageIs: comparePageNums,
+		userProfile: getTwitterUser,
+		personality: buildGraph
 		});
 
 //  Template.client.onRendered(function() 
@@ -102,33 +152,17 @@ if (Meteor.isClient) {
 //  );
   
   Template.client.events({
-	    'click #lookitup': function (event) 
-	    	{
-	    	event.preventDefault(); // We'll handle it
-	    	event.stopPropagation();
-	    	// Look up the client's twitter handle
-	    	document.getElementById("overlay").style.visibility ="visible";
-	    	document.getElementById("twitter-handle-lookup").className += "slideExpandUp";
-	    	return false;
-	    	},
-	    'click #close': function ()
-	    	{
-	    	document.getElementById("overlay").style.visibility ="hidden";
-	    	document.getElementById("twitter-handle-lookup").className =
-	    		   document.getElementById("twitter-handle-lookup").className.replace
-	    		      ( /(?:^|\s)slideExpandUp(?!\S)/g , '' )
-	    	},
+	    'click #lookitup': openLookup,
+	    'click #close': closeLookup,
 	    'blur #clientTwitter': getTwitterUser
-	    
 	  });
 
+  Template.page.events({
+	    'click #fetch': bumpPage
+  });
+  
   Template.nav.events({
-	'click a': function(event, template) {
-		if (event.target.name=="prev")
-			decreasePageNumber();//slide in previous page
-		else if (event.target.name=="next")
-			increasePageNumber();//slide in next page
-	}  
+	'click a': bumpPage  
   });
 }
 if (Meteor.isServer) {
